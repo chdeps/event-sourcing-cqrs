f92a5ac68f6f01c7159f494ce814fcaf796a8181
Alban Depretz <alband@bam.tech>
2019-04-19 12:46:53 +0200

    Save article in DB

--- src/commands/handlers/create-article.handler.ts
+++ src/commands/handlers/create-article.handler.ts
@@ -1,11 +1,17 @@
 import { CreateArticleCommand } from '../interfaces/create-article.command';
 import { CommandHandler, ICommandHandler, EventPublisher } from '@nestjs/cqrs';
 import { ArticleEntity, createArticle } from '../../article.entity';
+import { EntityManager } from 'typeorm';
 
 @CommandHandler(CreateArticleCommand)
 export class CreateArticleHandler implements ICommandHandler<CreateArticleCommand> {
-  constructor(private readonly eventPublisher: EventPublisher) {}
+  constructor(private readonly eventPublisher: EventPublisher, private readonly entityManager: EntityManager) {}
+  private articleRepository = this.entityManager.getRepository(ArticleEntity);
   public async execute(command: CreateArticleCommand): Promise<ArticleEntity> {
+    // TODO : Move logic else where
+    const savedArticle = await this.articleRepository.create(command.articleDto);
+    this.articleRepository.save(savedArticle);
+    // Dispatch event
     let newArticle = createArticle(command.articleDto);
     newArticle = this.eventPublisher.mergeObjectContext(newArticle);
     newArticle.commit();
